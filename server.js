96bc687a0d405b70e1d917850c8bf8b94ac28b51
Zaccheaus <zaccheausyisa00@gmail.com>
2022-01-21 16:32:24 -0800

    added config file for functions and search button

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
-const app = require("express")();
 const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 4000;
 
+const app = express();
 const initialPath = path.join(__dirname, "public");
 
 app.use(express.static(initialPath));
 
-app.get("/movie", (req, res) => {
-  res.send("hello");
+app.get("/", (req, res) => {
+  res.sendFile(path.join(initialPath, "index.html"));
+});
+
+app.get("/detail/:id", (req, res) => {
+  let id = req.params.id;
+  res.sendFile(path.join(initialPath, "detail.html"));
 });
 
 app.listen(PORT, () => console.log(`Listening on https://localhost:${PORT}`));
